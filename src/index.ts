4ba1984152b790a9330043818c4b48ce00085240
Talles Marques <tallesemanuel1@gmail.com>
2021-09-13 17:03:57 -0300

    Correções no CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "reflect-metadata";
 import * as express from "express"
+import * as cors from "cors";
 import { createConnection } from "typeorm"
 import { router } from "./router"
 
 createConnection().then(async () => {
 
   const app = express()
+  app.use(cors())
   app.use(express.json())
 
   app.use("/", router)
