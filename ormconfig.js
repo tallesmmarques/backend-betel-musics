eab76bfb66d560d2f40fb35cfc2a5d0814eef137
Talles Marques <tallesemanuel1@gmail.com>
2021-09-14 18:25:44 -0300

    Correções para deploy

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+require("dotenv")
+module.exports = {
+  "type": "postgres",
+  "host": process.env.HOST,
+  "port": process.env.PORTDATA,
+  "username": process.env.USERDATA,
+  "password": process.env.PASSWORD,
+  "database": process.env.DATABASE,
+  "synchronize": true,
+  "logging": false,
+  "entities": [
+    "dist/entity/**/*.js"
+  ],
+  "migrations": [
+    "dist/migration/**/*.js"
+  ],
+  "subscribers": [
+    "dist/subscriber/**/*.js"
+  ],
+  "cli": {
+    "entitiesDir": "dist/entity",
+    "migrationsDir": "dist/migration",
+    "subscribersDir": "dist/subscriber"
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
 import "reflect-metadata";
-import * as express from "express"
+import * as express from "express";
 import * as cors from "cors";
-import { createConnection } from "typeorm"
-import { router } from "./router"
+import * as dotenv from "dotenv";
+import { createConnection } from "typeorm";
+import { router } from "./router";
+
+dotenv.config({ path: __dirname + '/../.env' });
 
 createConnection().then(async () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
 
-  const app = express()
-  app.use(cors())
-  app.use(express.json())
+  app.use("/", router);
 
-  app.use("/", router)
-
-  const port = 8080
+  const port = process.env.PORT || 8080;
   app.listen(port, () => {
     console.log(`App is running in port ${port}`);
-
-  })
-
-}).catch(error => console.log(error));
+  });
+})
+  .catch((error) => console.log(error));
